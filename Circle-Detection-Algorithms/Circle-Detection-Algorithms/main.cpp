e888c62339163e2a4595c82cb6a92d85a88d2fce
Yuqing Lin <linyuqing97@gmail.com>
2020-02-22 17:27:38 -0500

    '' get the distance btw green and other colors'

--- Circle-Detection-Algorithms/Circle-Detection-Algorithms/main.cpp
+++ Circle-Detection-Algorithms/Circle-Detection-Algorithms/main.cpp
@@ -5,9 +5,9 @@
 #include <cassert>
 #include <cmath>
 #include <iostream>
+#include <fstream>
 #include <string>
 using namespace std;
-namespace {
 /**
  * @brief Convert BGR color to YCrCb color space.
  * @param bgr   Color (channel order BGR, range: [0, 255])
@@ -29,7 +29,6 @@ cv::Scalar bgr2ycrcb( cv::Scalar bgr )
     cout<<"Y: "<<Y <<"Cr: "<<Cr<<"Cb: "<<Cb<<endl;
     return cv::Scalar( Y, Cr, Cb, 0 /* ignored */ );
 }
-
 /**
  * @brief Separate foreground from background using simple chroma keying.
  *
@@ -44,8 +43,7 @@ cv::Scalar bgr2ycrcb( cv::Scalar bgr )
  *
  * [1]: https://en.wikipedia.org/wiki/Chroma_key#Programming
  */
-cv::Mat chromaKey( const cv::Mat3b & imageBGR, cv::Scalar chromaBGR, double tInner, double tOuter )
-{
+void chromaKey( const cv::Mat3b & imageBGR, cv::Scalar chromaBGR, double tInner, double tOuter){
     // Basic outline:
     //
     // 1. Convert the image to YCrCb.
@@ -56,7 +54,8 @@ cv::Mat chromaKey( const cv::Mat3b & imageBGR, cv::Scalar chromaBGR, double tInn
     //   * color distances between inner and outer threshold a linearly interpolated; mask value = [0, 255]
 
     assert( tInner <= tOuter );
-
+    ofstream outfile;
+outfile.open("/Users/yuqinglin/Desktop/Opencv-Circle-Detection/Circle-Detection-Algorithms/TestImages/result.txt");
     // Convert to YCrCb.
     assert( ! imageBGR.empty() );
     cv::Size imageSize = imageBGR.size();
@@ -70,110 +69,51 @@ cv::Mat chromaKey( const cv::Mat3b & imageBGR, cv::Scalar chromaBGR, double tInn
     cv::Mat1b mask = cv::Mat1b::zeros( imageSize );
     
     const cv::Vec3d key( chromaYCrCb[ 0 ], chromaYCrCb[ 1 ], chromaYCrCb[ 2 ] );
-
+    int countInner = 0;int countOutter = 0;
+    cout<<"iamge height"<<imageSize.height<<endl;
+    cout<<"image width"<<imageSize.width<<endl;
+    cout<<"total pixel"<<imageSize.height*imageSize.width<<endl;
     for ( int y = 0; y < imageSize.height; ++y )
     {
         for ( int x = 0; x < imageSize.width; ++x )
         {
             const cv::Vec3d color( imageYCrCb( y, x )[ 0 ], imageYCrCb( y, x )[ 1 ], imageYCrCb( y, x )[ 2 ] );
-            //std::cout<<"imageycrcv: "<<imageYCrCb(y,x)[0]<<" " <<imageYCrCb(y,x)[1]<<std::endl;
             double distance = cv::norm( key - color );
             if(distance<=20)
-            cout<<distance<<endl;
-
-            if ( distance < tInner )
-            {
-                // Current pixel is fully part of the background.
-                mask( y, x ) = 0;
-            }
-            else if ( distance > tOuter )
-            {
-                // Current pixel is fully part of the foreground.
-                mask( y, x ) = 255;
-            }
-            else
-            {
-                // Current pixel is partially part both, fore- and background; interpolate linearly.
-                // Compute the interpolation factor and clip its value to the range [0, 255].
-                double d1 = distance - tInner;
-                double d2 = tOuter   - tInner;
-                uint8_t alpha = static_cast< uint8_t >( 255. * ( d1 / d2 ) );
-
-                mask( y, x ) = alpha;
-            }
+                countInner++;
+            else countOutter++;
+            outfile<<x<<" "<<y<<" "<<distance<<" "<<endl;
         }
     }
-     
-    return mask;
+    cout<<"less than 20"<<countInner<<endl;
+    cout<<"grater than 20"<<countOutter<<endl;
+    return ;
 }
 
-cv::Mat3b replaceBackground( const cv::Mat3b & image, const cv::Mat1b & mask, cv::Scalar bgColor )
-{
-    cv::Size imageSize = image.size();
-    const cv::Vec3b bgColorVec( bgColor[ 0 ], bgColor[ 1 ], bgColor[ 2 ] );
-    cv::Mat3b newImage( image.size() );
 
-    for ( int y = 0; y < imageSize.height; ++y )
-    {
-        for ( int x = 0; x < imageSize.width; ++x )
-        {
-            uint8_t maskValue = mask( y, x );
-
-            if ( maskValue >= 255 )
-            {
-                newImage( y, x ) = image( y, x );
-            }
-            else if ( maskValue <= 0 )
-            {
-                newImage( y, x ) = bgColorVec;
-            }
-            else
-            {
-                double alpha = 1. / static_cast< double >( maskValue );
-                newImage( y, x ) = alpha * image( y, x ) + ( 1. - alpha ) * bgColorVec;
-            }
-        }
-    }
-
-    return newImage;
-}
-
-} // namespace
 
 int main()
 {
     std::string inputFilename = "/Users/yuqinglin/Desktop/Opencv-Circle-Detection/Circle-Detection-Algorithms/TestImages/xxx[1,2,3].jpg";
-    std::string maskFilename = "./mask.png";
-    std::string newBackgroundFilename = "./newBackground.png";
+
 
     // Load the input image.
-    cv::Mat3b input = cv::imread( inputFilename, cv::IMREAD_COLOR );
+    cv::Mat input = cv::imread( inputFilename, cv::IMREAD_COLOR );
 
-    if ( input.empty() )
-    {
+    if ( input.empty() ){
         std::cerr << "Input file <" << inputFilename << "> could not be loaded ... " << std::endl;
-
         return 1;
     }
-
     // Apply the chroma keying and save the output.
-    cv::Scalar chroma( 0, 0, 0, 0 );
+    cv::Scalar chroma( 0, 100, 80, 0 ); //tennis ball for rgb
     double tInner = 100.;
     double tOuter = 170.;
-    cv::Mat1b mask = chromaKey( input, chroma, tInner, tOuter );
-
-    cv::Mat3b newBackground = replaceBackground( input, mask, cv::Scalar( 0, 255, 0, 0 ) );
-
-    cv::imwrite( maskFilename, mask );
-    cv::imwrite( newBackgroundFilename, newBackground );
-
+    chromaKey( input, chroma, tInner, tOuter);
     cv::namedWindow( "input" );
     cv::imshow( "input", input );
-    cv::namedWindow( "mask" );
-    cv::imshow( "mask", mask );
-    cv::namedWindow( "new background" );
-    cv::imshow( "new background", newBackground );
     cv::waitKey( 0 );
 
     return 0;
 }
+
+
